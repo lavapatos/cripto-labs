d2a8634a0d719d31088a44f3bd990e2cef076448
Edicson <lucianosolarsalinas@gmail.com>
2026-09-19 01:44:39 -0300

    add: evidencias laboratorio 2

---
+++ lab-2/raw/curl_desde_inspector.sh
@@ -0,0 +1,16 @@
+curl 'http://127.0.0.1:6969/vulnerabilities/brute/?username=admin&password=password&Login=Login' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-US,en;q=0.9' \
+  -H 'Cache-Control: max-age=0' \
+  -b 'PHPSESSID=qiplsqh502ko6d0qlrc7n3jk23; security=low' \
+  -H 'Proxy-Connection: keep-alive' \
+  -H 'Referer: http://127.0.0.1:6969/vulnerabilities/brute/' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36' \
+  -H 'sec-ch-ua: "Not;A=Brand";v="8", "Chromium";v="150"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Linux"'
